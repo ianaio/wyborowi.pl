0789b9149a56b852b70aa7936e7ed66a3c5f8880
ianaio <dev@iana.io>
2025-03-10 18:27:55 +0000

    translation

--- src/data/products.ts
+++ src/data/products.ts
@@ -9,14 +9,14 @@ export interface Product {
 export const products: Product[] = [
   {
     id: '1',
-    name: 'Handgun Basics Course',
-    price: 99.99,
-    description: 'Learn the fundamentals of marksmanship from a Navy SEAL.',
+    name: 'Podstawowy Kurs posługiwania się bronią palną.',
+    price: 100,
+    description: 'Naucz się podstaw strzelectwa od żołnierza Navy SEAL.',
   },
   {
     id: '2',
-    name: 'Combat Shooting Course',
-    price: 149.99,
-    description: 'Master defensive shooting tactics.',
+    name: 'Kurs Strzelania Bojowego',
+    price: 200,
+    description: 'Opanuj taktykę defensywnego strzelania.'
   },
 ];
