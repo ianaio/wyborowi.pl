37aec3a279bc0ad612c052ac3bc6b5e47ef93db6
ianaio <dev@iana.io>
2025-03-10 18:40:36 +0000

    stripe publishable key update

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import App from './App';
 
-const stripePromise = loadStripe('your-publishable-key-here');
+const stripePromise = loadStripe('pk_test_51H8OFzCsXjkTPYjTMXoWpG1Kh2g4IGD8e2xQsUOXzBjf5IpunApfyF0fXdHFbV4AuCYUc3tVYpt6NywVhYzKiXuA00i3r3Ai7V');
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
