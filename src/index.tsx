37ba4ca64a62c5dcd80fa80f933c6c6abbc6bf0f
ianaio <dev@iana.io>
2025-03-11 10:48:24 +0000

    pk key update

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { loadStripe } from '@stripe/stripe-js';
 import { Elements } from '@stripe/react-stripe-js';
 import App from './App';
 
-const stripePromise = loadStripe('pk_test_51H8OFzCsXjkTPYjTMXoWpG1Kh2g4IGD8e2xQsUOXzBjf5IpunApfyF0fXdHFbV4AuCYUc3tVYpt6NywVhYzKiXuA00i3r3Ai7V');
+const stripePromise = loadStripe('pk_test_51R1O6yQ67xdNaVLCZFPKv5ANzlL5MTzqdfOvBUz7DzTTZ65i9RHEVjuZRCtaO1OkHZOmbQQUonxWvN43XBz7CSGj00i4se8PnA');
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
